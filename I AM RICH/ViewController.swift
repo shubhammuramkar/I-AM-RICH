50d2ea7e17f4d7fd7a0c3ec93427f36d02f118fa
Apple <shubhammuramkar96@gmail.com>
2018-08-04 17:37:03 +0530

    projects

---
+++ I AM RICH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by Apple on 15/01/18.
+//  Copyright © 2018 Shubham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
